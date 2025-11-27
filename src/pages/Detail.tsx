ab3f23343ded40a56ade198843d7492b8d871ac4
Djttt <77910823+Djttt@users.noreply.github.com>
2025-11-27 18:30:59 +0800

    Add github comments system by giscus

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,4 +1,6 @@
 import type { Dish } from "../types/menu"
+import Giscus from '@giscus/react'
+import  { giscusConfigureData } from "../types/giscus"
 
 export default function Detail(props: Dish) {
     
@@ -6,6 +8,9 @@ export default function Detail(props: Dish) {
         <div>
             <p>This is a detail page!</p>
             <p>{props.description}</p>
+            <Giscus  
+                {...giscusConfigureData}
+            />
         </div>
     )
 }
---
+++ src/types/giscus.ts
@@ -0,0 +1,13 @@
+
+export const giscusConfigureData = {
+    repo: "Djttt/campus-canteen-assets",
+    repoId: "R_kgDOQeAZRQ",
+    categoryId: "DIC_kwDOQeAZRc4CzHEr",
+    category: "Announcements",
+    mapping: "pathname",
+    reactionsEnabled: "1",
+    emitMetadata: "0",
+    inputPosition: "bottom",
+    // theme: "preferred_color_scheme",
+    lang: "zh-CN"
+};
